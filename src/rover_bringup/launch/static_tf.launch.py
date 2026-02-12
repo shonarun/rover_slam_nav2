29d2ecc629979cfd1ac37545bf32280636716aee
Shon Arun <ajkallidukil8173@gmail.com>
2026-02-12 14:54:55 +0530

    Modify static_tf.launch.py for flexibility

--- src/rover_bringup/launch/static_tf.launch.py
+++ src/rover_bringup/launch/static_tf.launch.py
@@ -1,19 +1,28 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
     return LaunchDescription([
+        DeclareLaunchArgument('x', default_value='0.1'),
+        DeclareLaunchArgument('y', default_value='0.0'),
+        DeclareLaunchArgument('z', default_value='0.2'),
+        DeclareLaunchArgument('roll', default_value='0.0'),
+        DeclareLaunchArgument('pitch', default_value='0.0'),
+        DeclareLaunchArgument('yaw', default_value='0.0'),
+
         Node(
             package='tf2_ros',
             executable='static_transform_publisher',
             name='base_to_laser_tf',
             arguments=[
-                '--x', '0.1',
-                '--y', '0.0',
-                '--z', '0.2',
-                '--roll', '0.0',
-                '--pitch', '0.0',
-                '--yaw', '0.0',
+                '--x', LaunchConfiguration('x'),
+                '--y', LaunchConfiguration('y'),
+                '--z', LaunchConfiguration('z'),
+                '--roll', LaunchConfiguration('roll'),
+                '--pitch', LaunchConfiguration('pitch'),
+                '--yaw', LaunchConfiguration('yaw'),
                 '--frame-id', 'base_link',
                 '--child-frame-id', 'base_laser',
             ]
