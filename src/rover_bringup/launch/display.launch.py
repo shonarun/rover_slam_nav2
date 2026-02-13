d146d3b8dd94a186ce0421a545b28e64d28095b0
Shon Arun <ajkallidukil8173@gmail.com>
2026-02-13 23:58:39 +0530

    Create display.launch.py in the launch folder

---
+++ src/rover_bringup/launch/display.launch.py
@@ -0,0 +1,12 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import Command, LaunchConfiguration
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+import os
+
+def generate_launch_description():
+    return LaunchDescription([
+        
+    ])
