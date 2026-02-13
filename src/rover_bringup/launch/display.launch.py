70dbcee06037b5ec36e32e678a19c6e19865eb1e
Shon Arun <ajkallidukil8173@gmail.com>
2026-02-13 23:59:12 +0530

    Add key variables

--- src/rover_bringup/launch/display.launch.py
+++ src/rover_bringup/launch/display.launch.py
@@ -7,6 +7,10 @@ from launch_ros.substitutions import FindPackageShare
 import os
 
 def generate_launch_description():
+    pkg_share = FindPackageShare(package='sam_bot_description').find('sam_bot_description')
+    default_model_path = os.path.join(pkg_share, 'src', 'description', 'sam_bot_description.urdf')
+    default_rviz_config_path = os.path.join(pkg_share, 'rviz', 'config.rviz')
+
     return LaunchDescription([
         
     ])
