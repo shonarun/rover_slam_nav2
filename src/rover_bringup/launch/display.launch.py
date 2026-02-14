626bc32e757b6251934e5416be4e29de5e693ab4
Shon Arun <ajkallidukil8173@gmail.com>
2026-02-14 09:22:28 +0530

    Update key variables

--- src/rover_bringup/launch/display.launch.py
+++ src/rover_bringup/launch/display.launch.py
@@ -7,9 +7,10 @@ from launch_ros.substitutions import FindPackageShare
 import os
 
 def generate_launch_description():
-    pkg_share = FindPackageShare(package='sam_bot_description').find('sam_bot_description')
-    default_model_path = os.path.join(pkg_share, 'src', 'description', 'sam_bot_description.urdf')
-    default_rviz_config_path = os.path.join(pkg_share, 'rviz', 'config.rviz')
+    rover_bringup_pkg_share = FindPackageShare(package='rover_bringup').find('rover_bringup')
+    rover_description_pkg_share = FindPackageShare(package='rover_description').find('rover_description')
+    default_model_path = os.path.join(rover_description_pkg_share, 'urdf', 'rover.urdf')
+    default_rviz_config_path = os.path.join(rover_bringup_pkg_share, 'rviz', 'config.rviz')
 
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
