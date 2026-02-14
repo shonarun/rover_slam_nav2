91db8834a67d07ce86956013386484df20cfc35f
Shon Arun <ajkallidukil8173@gmail.com>
2026-02-14 18:56:43 +0530

    rename bot to rover

--- src/rover_bringup/launch/display.launch.py
+++ src/rover_bringup/launch/display.launch.py
@@ -50,7 +50,7 @@ def generate_launch_description():
         launch_arguments={
             'world': 'my_world',
             'topic': '/robot_description',
-            'entity_name': 'sam_bot',
+            'entity_name': 'rover',
             'z': '0.65',
         }.items(),
     )
