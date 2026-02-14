55660d3ee7d03c9b9eb706835cb648f66e3f3842
Shon Arun <ajkallidukil8173@gmail.com>
2026-02-14 18:07:43 +0530

    Remove unused imports

--- src/rover_bringup/launch/bringup.launch.py
+++ src/rover_bringup/launch/bringup.launch.py
@@ -3,7 +3,6 @@ from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 import os
-from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
