a37bff980373359506c8c608c2bd3eb6a3e70b4a
Shon Arun <ajkallidukil8173@gmail.com>
2026-02-14 18:02:22 +0530

    Add nav2_params

--- src/rover_bringup/launch/bringup.launch.py
+++ src/rover_bringup/launch/bringup.launch.py
@@ -3,9 +3,19 @@ from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 import os
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
 
 
 def generate_launch_description():
+    # Declare argument for params file
+    nav2_params_file = LaunchConfiguration('nav2_params_file')
+    declare_params = DeclareLaunchArgument(
+        'params_file',
+        default_value='nav2_params.yaml',
+        description='Full path to the ROS2 parameters file to use for Nav2'
+    )
+
     pkg_dir = get_package_share_directory('rover_bringup')
     static_tf_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
